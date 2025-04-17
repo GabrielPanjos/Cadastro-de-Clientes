983effe47cda7fbf9bc2c0bd90f0223340a6fe13
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-04-17 19:27:25 -0300

    Add files via upload

---
+++ Cadastro de Clientes/static/js/script.js
@@ -0,0 +1,30 @@
+btnGeral = document.getElementById("btn-geral");
+btnEndereco = document.getElementById("btn-endereco");
+
+function mostrarSecao(secaoId) {
+    document.getElementById("geral").style.display = "none";
+    document.getElementById('endereco').style.display = "none";
+    document.getElementById('h2-contatos').style.display = "none";
+
+    document.getElementById(secaoId).style.display = "";
+    if (secaoId == "geral") {
+        document.getElementById('h2-contatos').style.display = "";
+        document.getElementById("btn-geral").style.backgroundColor = " #ffffff"
+        document.getElementById("btn-geral").style.color = " #5634E0"
+        document.getElementById("btn-endereco").style.backgroundColor = " #5634E0"
+        document.getElementById("btn-endereco").style.color = " #ffffff"
+    } else {
+        document.getElementById("btn-endereco").style.backgroundColor = " #ffffff"
+        document.getElementById("btn-endereco").style.color = " #5634E0"
+        document.getElementById("btn-geral").style.backgroundColor = " #5634E0"
+        document.getElementById("btn-geral").style.color = " #ffffff"
+    }
+}
+
+document.getElementById('btn-geral').addEventListener("click", () => {
+    mostrarSecao("geral")
+})
+
+document.getElementById('btn-endereco').addEventListener("click", () => {
+    mostrarSecao("endereco")
+})
