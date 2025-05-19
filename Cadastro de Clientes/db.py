b7e4fec0db836ce80bf3bc5456fdf0adcda79e06
Gabriel Pereira <pereiradosanjosgabriel@gmail.com>
2025-05-19 15:45:40 -0300

    Update db.py

--- Cadastro de Clientes/db.py
+++ Cadastro de Clientes/db.py
@@ -4,7 +4,7 @@ def get_db_connection():
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Mamae38.",
+        password="toor",
         database="school_universe"
     )
     return conn
